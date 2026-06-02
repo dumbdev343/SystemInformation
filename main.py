eba22bba25159448a5bc3d6f2d6c8875a625f778
DumbDev343 <62670550+dumbdev343@users.noreply.github.com>
2026-06-02 13:25:40 +1200

    Broken but perhaps fixing at the same time

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import sys
 import random
 pygame.init()
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255),)
-font = pygame.font.SysFont(None, 36)
 screen = pygame.display.set_mode((1280,720))
+font = pygame.font.SysFont(screen, 36)
+
 pygame.display.set_caption("System Infomation")
 assci = r"""
   ___         _               ___       __                    _   _          
@@ -17,7 +18,7 @@ assci = r"""
  |___/\_, /__/\__\___|_|_|_| |___|_||_|_| \___/_| |_|_|_\__,_|\__|_\___/_||_|
       |__/                                                                                                                                                
 """
-font.render(f"You are running {platform.system()}!", True, (128, 253, 123), (0, 0, 0))
+
 
 os.system("clear")
 print(assci)
@@ -29,15 +30,23 @@ if platform.system() == ("Linux"):
         print("You are running on the Distro Debian!")
 if platform.system() == ("Darwin"):
     print("You are running on Darwin/Linux!")
-print(f"Your CPU is the {cpuinfo.get_cpu_info()["brand_raw"]} and it has {os.cpu_count()} cores!")
 screen.fill("white")
+font.render(f"You are running {platform.system()}!", True, (128, 253, 123), (0, 0, 0))
+print(f"Your CPU is the {cpuinfo.get_cpu_info()["brand_raw"]} and it has {os.cpu_count()} cores!")
+
 pygame.display.flip()
 print(" ")
 print(" ")
 
-install = input("Would you like to install any programs? ")
-if "yes" in install:
-    if platform.system() == ("Windows"):
-        pkgname = input("What package would you like to install? (Using WinGet): ")
-        os.system(f"winget install {pkgname}")
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
+#install = input("Would you like to install any programs? ")
+#if "yes" in install:
+    #if platform.system() == ("Windows"):
+    #    pkgname = input("What package would you like to install? (Using WinGet): ")
+     #   os.system(f"winget install {pkgname}")
+#
